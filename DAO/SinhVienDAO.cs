a619285ce0da34d184e72fa76b7522dde291651a
Quan <sieutihons10@users.noreply.github.com>
2020-07-24 10:51:42 +0700

    update SinhVIen

---
+++ DAO/SinhVienDAO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+namespace DAO
+{
+    public class SinhVienDAO
+    {
+        public static SinhVienDTO ConvertToDTO(DataRow dr) {
+            SinhVienDTO sv = new SinhVienDTO();
+            sv.Ten_SV = dr["Ten_SV"].ToString();
+            sv.Ma_SV = dr["Ma_SV"].ToString();
+            sv.Ma_Lop = dr["Ma_Lop"].ToString();
+            sv.SoNgayHoc = Convert.ToInt32(dr["SoNgayHoc"]);
+            sv.SoNgayVang = Convert.ToInt32(dr["SoNgayVang"]);
+            return sv;
+         }
+        public static List<SinhVienDTO> LayDSSV()
+        {
+            string query = " SELECT * FROM ThongTinSV";
+            SqlParameter[] param = new SqlParameter[0];
+            DataTable dtbKetQua = DataProvider.ExecuteSelectQuery(query,param);
+            List<SinhVienDTO> lstSinhVien = new List<SinhVienDTO>();
+            foreach(DataRow dr in dtbKetQua.Rows)
+            {
+                lstSinhVien.Add(ConvertToDTO(dr);
+            }
+            return lstSinhVien;
+        }
+
+    }
+}
---
+++ FaceAPI/DTO/SinhVienDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class SinhVienDTO
+    {
+        private String ma_SV;
+        private String ten_SV;
+        private String ma_Lop;
+        private int soNgayHoc;
+        private int soNgayVang;
+
+        public string Ma_SV { get => Ma_SV; set => Ma_SV = value; }
+        public string Ten_SV { get => Ten_SV; set => Ten_SV = value; }
+        public string Ma_Lop { get => Ma_Lop; set => Ma_Lop = value; }
+        public int SoNgayHoc { get => SoNgayHoc; set => SoNgayHoc = value; }
+        public int SoNgayVang { get => SoNgayVang; set => SoNgayVang = value; }
+    }
+}
